8069b4b502b41a504c568e2e337cb339e3d3aabd
mangesh <mbchate08@gmail.com>
2024-04-09 13:19:53 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
 import Companysetup from "./components/Companysetup";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserManagement from "./components/UserManagement";
 import Outletsetup from "./components/Outletsetup";
 
@@ -11,18 +11,19 @@ function App() {
   const [sidebarToggle, setSidebarToggle] = useState(false);
   return (
     <Router className="flex justify-right">
+
+
+      <Sidebar sidebarToggle={sidebarToggle} />
+      <Dashboard
+        sidebarToggle={sidebarToggle}
+        setSidebarToggle={setSidebarToggle}
+      />
+
       <Routes>
-        <Route path="Companysetup" element={<Companysetup />} />
-        <Route path="UserManagement" element={<UserManagement />} />
-        <Route path="Outletsetup" element={<Outletsetup />} />
+        <Route path="/Companysetup" element={<Companysetup />} />
+        <Route path="/UserManagement" element={<UserManagement />} />
+        <Route path="/Outletsetup" element={<Outletsetup />} />
       </Routes>
-      <div className="flex">
-        <Sidebar sidebarToggle={sidebarToggle} />
-        <Dashboard
-          sidebarToggle={sidebarToggle}
-          setSidebarToggle={setSidebarToggle}
-        />
-      </div>
     </Router>
   );
 }
