8bd52c0c81e570954171091084b8190d4729a622
Sainath Atmaram Gaikwad <89898283+SainathGaikwad@users.noreply.github.com>
2024-04-09 13:54:24 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Sidebar from "./components/Sidebar";
 import Companysetup from "./components/Companysetup";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import UserManagement from "./components/UserManagement";
 import Outletsetup from "./components/Outletsetup";
 
 function App() {
   const [sidebarToggle, setSidebarToggle] = useState(false);
   return (
-    <Router className="flex justify-right">
-      <Sidebar sidebarToggle={sidebarToggle} />
-      <Dashboard
-        sidebarToggle={sidebarToggle}
-        setSidebarToggle={setSidebarToggle}
-      />
-
+    <div className="flex justify-right">
+      <Navbar></Navbar>
       <Routes>
-        <Route path="/Companysetup" element={<Companysetup />} />
-        <Route path="/UserManagement" element={<UserManagement />} />
-        <Route path="/Outletsetup" element={<Outletsetup />} />
+        <Route path="Companysetup" element={<Companysetup />} />
+        <Route path="UserManagement" element={<UserManagement />} />
+        <Route path="Outletsetup" element={<Outletsetup />} />
       </Routes>
-    </Router>
+      <div className="flex">
+        <Sidebar sidebarToggle={sidebarToggle} />
+        <Dashboard
+          sidebarToggle={sidebarToggle}
+          setSidebarToggle={setSidebarToggle}
+        />
+      </div>
+    </div>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
-    <React.StrictMode>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
